635b18d024bcb26d03464641c049e40551ca222e
YUhyfe <prefil2005@gmail.com>
2026-04-23 23:23:14 +0200

    feat(project): add findAllByStatus query method

--- src/main/java/com/yuhyfe/thebuggerbot/project/ProjectRepository.java
+++ src/main/java/com/yuhyfe/thebuggerbot/project/ProjectRepository.java
@@ -3,6 +3,7 @@ package com.yuhyfe.thebuggerbot.project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,5 +12,7 @@ public interface ProjectRepository extends JpaRepository<Project, Long> {
     Optional<Project> findByName(String name);
 
     boolean existsByName(String name);
+
+    List<Project> findAllByStatus(Project.ProjectStatus status);
 
 }
